8567e958f28dd690dba90763bd0c121f613e2208
rwhuang <rwhuang@lc-bio.com>
2020-05-22 10:27:05 +0800

    实现服务端消息广播

--- socket/client/main.go
+++ socket/client/main.go
@@ -7,19 +7,28 @@ import (
 )
 
 func main() {
-	var msg string
 	conn, err := net.Dial("tcp", ":8888")
 	defer conn.Close()
 	checkError(err)
 
+	go func() {
+		for {
+			buf := make([]byte, 1024)
+			n, err := conn.Read(buf)
+			checkError(err)
+			fmt.Println(string(buf[:n]))
+		}
+	}()
+
+	var msg string
 	for {
-		fmt.Print("输入信息:")
 		fmt.Scanln(&msg)
 
 		// 发送数据
 		_, err = conn.Write([]byte(msg))
 		checkError(err)
 	}
+
 }
 
 func checkError(err error) {
--- socket/server/main.go
+++ socket/server/main.go
@@ -9,33 +9,49 @@ import (
 const MAXINUM_CLIENT = 10
 
 func main() {
+	clients := make(map[string]net.Conn)
+
 	listener, err := net.Listen("tcp", ":8888")
 	checkError(err)
 	defer listener.Close()
 
 	fmt.Println("服务器已启动，等待客户端建立连接...")
 
-	count := make(chan int, MAXINUM_CLIENT)
+	countCh := make(chan int, MAXINUM_CLIENT)
+	msgCh := make(chan string)
+
+	//监控消息通道,有新消息时发送到客户端
+	go func() {
+		for {
+			message := <-msgCh
+			for _, conn := range clients {
+				conn.Write([]byte(message))
+			}
+		}
+	}()
 
 	for {
-		count <- 1
+		countCh <- 1
 		go func() {
 			conn, err := listener.Accept()
 			checkError(err)
 			defer conn.Close()
+
 			ip := conn.RemoteAddr().String()
-			fmt.Println("系统消息：" + ip + "加入聊天室")
+			clients[ip] = conn
+			defer delete(clients, ip)
+			msgCh <- "系统消息：" + ip + "加入聊天室"
 			for {
 				buf := make([]byte, 1024) // 创建1024大小的缓冲区，用于read
 				n, err := conn.Read(buf)
 				if err != nil {
-					fmt.Println("系统消息：" + ip + "退出了聊天室")
+					msgCh <- "系统消息：" + ip + "退出了聊天室"
 					break
 				}
-				fmt.Println(ip+":", string(buf[:n])) // 读多少，打印多少。
+				msgCh <- "@" + ip + ":" + string(buf[:n]) // 读取内容放入消息通道
 			}
 
-			<-count
+			<-countCh
 		}()
 	}
 }
